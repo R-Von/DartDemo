ef64c1ba724d880db7043989851a94752ca2d8b1
R-Von <778615648@qq.com>
2020-06-15 15:47:44 +0800

    feat():first dart demo

---
+++ demo1.dart
@@ -0,0 +1,9 @@
+// 定义一个函数
+printInteger(int aNumber){
+  print('The number is $aNumber ');   //输出到控制台
+}
+// 应用从这里开始执行
+main(){
+  var number = 42; //声明 并初始化 一个变量
+   printInteger(number); //调用函数  
+}
